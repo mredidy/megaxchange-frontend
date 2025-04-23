08978bf68e1eddd2bd1318d26664d60b246cc4f9
Mister Edidy <100961974+mredidy@users.noreply.github.com>
2025-04-23 16:30:52 +0000

    Yea

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-}
+  allowedDevOrigins: [
+    "https://8ffc0398-9973-4612-b25b-9b81b9c790fa-00-20h2dvssn3lju.worf.replit.dev"
+  ],
+};
+
+module.exports = nextConfig;
